4339e470859ac30a383282930d9eeebee15548e3
H3A4K <alex.perlock@gmail.com>
2026-03-25 15:50:54 -0400

    dbh connect

--- dbh.php
+++ dbh.php
@@ -1,2 +1,13 @@
 <?php
 
+try {
+    $dbh = new PDO(
+        "mysql:host=localhost;dbname=_db",
+        "root",
+        "",
+    );
+} catch (Exception $e) {
+    die("ERROR: COULDN'T CONNECT TO DATABASE {$e->getMessage()}");
+}
+
+return $dbh;
