e2a71bcc59ca1bb18e337ea05aec6529047de221
Neel <paten164+github@mcmaster.ca>
2026-03-30 15:29:50 -0400

    add fetching menu items from database

---
+++ JS/menu.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", function() {
+
+    const menu = document.getElementById("menu");
+
+    function handleMenu(data) {
+
+        menu.innerHTML = "";
+
+        for (const product of data) {
+            const item = document.createElement("div");
+            item.className = "menuitem";
+
+            const img = document.createElement("img");
+            img.src = "../assets/images/" + product.productimg;
+
+            const br = document.createElement("br");
+
+            const desc = document.createElement("div");
+            desc.className = "desc";
+
+            const h2 = document.createElement("h2");
+            h2.textContent = product.productname;
+
+            const p = document.createElement("p");
+            p.textContent = product.productdesc;
+
+            desc.append(h2, p);
+            item.append(img, br, desc); 
+            menu.appendChild(item);
+        }
+
+    }
+
+    fetch("../assets/php/menu.php")
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Server returned " + response.status);
+            }
+            return response.json()
+        })
+        .then(handleMenu);
+        
+
+
+})
