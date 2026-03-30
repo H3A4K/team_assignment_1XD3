d7c459211c90316ca82b8214eeb427f46f9ca806
Neel <paten164+github@mcmaster.ca>
2026-03-30 15:43:29 -0400

    make interface display prices

--- JS/menu.js
+++ JS/menu.js
@@ -21,10 +21,13 @@ window.addEventListener("load", function() {
             const h2 = document.createElement("h2");
             h2.textContent = product.productname;
 
+            const h3 = document.createElement("h3");
+            h3.textContent = "$" + product.price;
+
             const p = document.createElement("p");
             p.textContent = product.productdesc;
 
-            desc.append(h2, p);
+            desc.append(h2, h3, p);
             item.append(img, br, desc); 
             menu.appendChild(item);
         }
