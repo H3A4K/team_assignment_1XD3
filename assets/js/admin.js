733afc5b147aa760be24384dc7c70a403db841a5
UHA242 <kat.uhalova@outlook.com>
2026-04-03 19:50:03 -0400

    admin module changes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,7 @@
+
 let allProducts = [];
+let allOrders = [];
+let allPromoCodes = [];
 
 function renderProductTable(data) {
     const productTable = document.getElementById("product-table");
@@ -7,7 +10,7 @@ function renderProductTable(data) {
 
     if (data.length === 0) {
         const empty = document.createElement("p");
-        empty.id = "menu-empty";
+        empty.id = "products-empty";
         empty.textContent = "No products found.";
         productTable.appendChild(empty);
         return;
@@ -80,9 +83,169 @@ function renderProductTable(data) {
     productTable.appendChild(table);
 }
 
-window.addEventListener("load", function () {
-    console.log("load");
-    fetch("../assets/php/menu.php")
+function renderOrderTable(data) {
+    const orderTable = document.getElementById("order-table");
+
+    orderTable.innerHTML = "";
+
+    if (data.length === 0) {
+        const empty = document.createElement("p");
+        empty.id = "orders-empty";
+        empty.textContent = "No orders found.";
+        productTable.appendChild(empty);
+        return;
+    }
+
+    let table = document.createElement("table");
+    table.className = "table";
+
+    const headerRow = document.createElement("tr");
+    let name = document.createElement("th");
+    name.innerText = "Order ID";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Account ID";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Order Date";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Address";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Status";
+    headerRow.appendChild(name);
+
+    table.appendChild(headerRow);
+
+    for (const order of data) {
+
+        const orderRow = document.createElement("tr");
+        let cell = document.createElement("td");
+
+        cell.innerText = order.orderID;
+        orderRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        cell.innerText = order.accountID;
+        orderRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        cell.innerText = order.orderDate;
+        orderRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        cell.innerText = order.address;
+        orderRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        if (order.fullfilled === 1) {
+            cell.innerText = "Completed";
+        }
+        else if (order.fullfilled === 0) {
+            cell.innerText = "Incomplete";
+        }
+        orderRow.appendChild(cell);
+
+        table.appendChild(orderRow);
+    }
+
+    orderTable.appendChild(table);
+}
+
+function renderPromoCodeTable(data) {
+    const promoCodeTable = document.getElementById("promocodes-table");
+
+    promoCodeTable.innerHTML = "";
+
+    if (data.length === 0) {
+        const empty = document.createElement("p");
+        empty.id = "promocodes-empty";
+        empty.textContent = "No promo codes found.";
+        promoCodeTable.appendChild(empty);
+        return;
+    }
+
+    let table = document.createElement("table");
+    table.className = "table";
+
+    const headerRow = document.createElement("tr");
+    let name = document.createElement("th");
+    name.innerText = "Promo Code ID";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Promo Code";
+    headerRow.appendChild(name);
+
+    // name = document.createElement("th");
+    // name.innerText = "Discount Type";
+    // headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Discount Value";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Status";
+    headerRow.appendChild(name);
+
+    name = document.createElement("th");
+    name.innerText = "Expiry Date";
+    headerRow.appendChild(name);
+
+    table.appendChild(headerRow);
+
+    for (const promocodes of data) {
+
+        const promoCodeRow = document.createElement("tr");
+        let cell = document.createElement("td");
+
+        cell.innerText = promocodes.promoID;
+        promoCodeRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        cell.innerText = promocodes.promoCode;
+        promoCodeRow.appendChild(cell);
+
+        // cell = document.createElement("td");
+        // cell.innerText = promocodes.discountType;
+        // promoCodeRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        if (promocodes.discountType === "percentage") {
+            cell.innerText = promocodes.discountValue + "%";
+        }
+        else if (promocodes.discountType === "fixed") {
+            cell.innerText = "$" + parseFloat(promocodes.discountValue).toFixed(2);
+        }
+        promoCodeRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        if (promocodes.active === 1) {
+            cell.innerText = "Active";
+        }
+        else if (promocodes.active === 0) {
+            cell.innerText = "Inoperative";
+        }
+        promoCodeRow.appendChild(cell);
+
+        cell = document.createElement("td");
+        cell.innerText = promocodes.expiryDate;
+        promoCodeRow.appendChild(cell);
+
+        table.appendChild(promoCodeRow);
+    }
+
+    promoCodeTable.appendChild(table);
+}
+
+function getAllProducts() {
+    fetch("../assets/php/admin.php?getAllProducts")
         .then(function (response) {
             if (!response.ok) {
                 throw new Error("Server returned " + response.status);
@@ -93,6 +256,48 @@ window.addEventListener("load", function () {
             allProducts = data;
             renderProductTable(allProducts);
         });
+}
 
-    
+function getAllOrders() {
+    fetch("../assets/php/admin.php?getAllOrders")
+    .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Server returned " + response.status);
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            allOrders = data;
+            renderOrderTable(allOrders);
+        });
+}
+
+function getAllPromoCodes() {
+    fetch("../assets/php/admin.php?getAllPromoCodes")
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error("Server returned " + response.status);
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            allPromoCodes = data;
+            renderPromoCodeTable(allPromoCodes);
+        });
+}
+
+
+window.addEventListener("load", function () {
+    console.log("get products");
+    getAllProducts();
+});
+
+window.addEventListener("load", function () {
+    console.log("get orders");
+    getAllOrders();
+});
+
+window.addEventListener("load", function () {
+    console.log("get promo codes");
+    getAllPromoCodes();
 });
