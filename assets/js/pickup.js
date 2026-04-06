a855674e1ae776cce9519deddbcea45272be558b
H3A4K <alex.perlock@gmail.com>
2026-04-06 14:58:20 -0400

    re

--- assets/js/pickup.js
+++ assets/js/pickup.js
@@ -6,14 +6,19 @@
  */
 
 window.addEventListener("load", function() {
-    const display = document.getElementById("time");
 
     function success(json) {
+        if (!json) {
+            const main = document.querySelector("main");
+            main.innerHTML = "";
+            return;
+        }
+        const display = document.getElementById("time");
         // JSON.parse(json);
         display.innerHTML = json;
     }
 
     fetch("../assets/php/pickup.php")
-        .then(response => response.json())
+        .then(response => response.text())
         .then(success);
 });
