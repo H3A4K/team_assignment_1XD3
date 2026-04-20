e479475d24a642463fae59b0cde00fd761cf1b2b
Neel P. <paten164+github@mcmaster.ca>
2026-04-19 23:29:00 -0400

    clean up unused code, add minus sign to discount in pickup page

--- assets/php/pickup.php
+++ assets/php/pickup.php
@@ -336,7 +336,7 @@ function formatOrder(array $items, float $discount): string {
     $out .= table_row("Subtotal", null, null, $total, "line-break");
     
     if ($discount !== (float) 0 || $discount === null) {
-        $out .= table_row("Discount", null, null, $discount, null);
+        $out .= table_row("Discount", null, null, "−" . $discount, null);
         $total -= $discount;
     }
 
