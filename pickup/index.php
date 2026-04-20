e0237c0f1df63358dfd015c5134c5056ec7d6f44
H3A4K <alex.perlock@gmail.com>
2026-04-19 20:19:05 -0400

    table + 2

--- pickup/index.php
+++ pickup/index.php
@@ -72,10 +72,10 @@ session_start();
     </nav>
 
     <main>
-        <h1><span id="time-label">Estimated Wait Time</span>: <span id="time">--</span> minutes</h1>
+        
+        <h1><div><span id="time-label">Estimated Wait Time</span>: <span id="time">--</span> minutes </div><div id="pickup-table"></div></h1>
         <p id="pickup-detail"></p>
         <p id="pickup-address"></p>
-        <div id="pickup-table"></div>
     </main>
 
     <footer class="site-footer">
